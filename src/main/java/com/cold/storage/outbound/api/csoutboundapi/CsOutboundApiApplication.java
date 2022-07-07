74b458589baccf6635169d74da125f9ca99a1cd6
Pankaj <pankaj.rajput07052@gmail.com>
2022-07-07 22:36:36 +0530

    first commit

---
+++ src/main/java/com/cold/storage/outbound/api/csoutboundapi/CsOutboundApiApplication.java
@@ -0,0 +1,13 @@
+package com.cold.storage.outbound.api.csoutboundapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsOutboundApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsOutboundApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cold/storage/outbound/api/csoutboundapi/CsOutboundApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cold.storage.outbound.api.csoutboundapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsOutboundApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
